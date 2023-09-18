d1bc5e30c6899a7f1cd94b3ab9ed1a2f591ef564
Akbar Nafisa <akbarnj788@gmail.com>
2023-09-18 10:47:58 +0700

    add form component

---
+++ src/components/forms/form.tsx
@@ -0,0 +1,42 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+  UseFormProps,
+} from "react-hook-form";
+import { AnyObjectSchema } from "yup";
+import Alert from "../common/alert";
+
+type FormProps<TFormValues extends FieldValues> = {
+  onSubmit: SubmitHandler<TFormValues>;
+  children: React.ReactNode;
+  schema?: AnyObjectSchema;
+  alert?: boolean; // use alert message on top of the form
+} & UseFormProps<TFormValues>;
+
+// can also use extend FieldValues
+export default function Form<TFormValues extends Record<string, any>>({
+  onSubmit,
+  schema,
+  defaultValues,
+  mode,
+  reValidateMode = "onChange",
+  alert = true,
+  children,
+}: FormProps<TFormValues>) {
+  const methods = useForm({
+    defaultValues,
+    mode,
+    reValidateMode,
+    resolver: schema && yupResolver(schema),
+  });
+
+  return (
+    <FormProvider {...methods}>
+      {alert && <Alert />}
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+}
